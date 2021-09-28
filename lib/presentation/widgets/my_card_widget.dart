c0ff4868a7a9cd1b64ec9a7635415af71501de52
Ivy-Walobwa <ivywalobwa@gmail.com>
2021-09-28 10:01:25 +0300

    Implement Scale

--- lib/presentation/widgets/my_card_widget.dart
+++ lib/presentation/widgets/my_card_widget.dart
@@ -13,6 +13,8 @@ class MyCardWidget extends StatefulWidget {
 class _MyCardWidgetState extends State<MyCardWidget> {
   Color bgColor = Colors.yellow;
   bool makeCircular = false;
+  double _scaleFactor = 0.5;
+  double _baseScaleFactor = 0.5;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -26,13 +28,29 @@ class _MyCardWidgetState extends State<MyCardWidget> {
           makeCircular = !makeCircular;
         });
       },
-      child:   Card(
-        shape: makeCircular? const CircleBorder(): const RoundedRectangleBorder(),
-        child: const SizedBox(
-          height: 300,
-          width: 300,
+      onScaleStart: (details){
+        _baseScaleFactor = _scaleFactor;
+
+      },
+      onScaleUpdate: (details){
+        setState(() {
+          _scaleFactor = _baseScaleFactor * details.scale;
+        });
+      },
+      onScaleEnd: (details){
+        // return to initial scale
+        _scaleFactor = _baseScaleFactor;
+      },
+      child:   Transform.scale(
+        scale: _scaleFactor,
+        child: Card(
+          shape: makeCircular? const CircleBorder(): const RoundedRectangleBorder(),
+          child: const SizedBox(
+            height: 300,
+            width: 300,
+          ),
+          color: bgColor,
         ),
-        color: bgColor,
       ),
     );
   }
